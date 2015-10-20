a563080ba4d22b746284dda88efee37259a79da9
jeanepaul <jeanepaul@gmail.com>
2015-10-20 11:04:58 +0900

    Initial Commit

---
+++ learnJap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  learnJap
+//
+//  Created by ジエーンポール ソリバ on 10/20/15.
+//  Copyright © 2015 ジエーンポール ソリバ. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
